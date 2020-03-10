13f89bda177ee47d17b18ad392cf420b543e2eaa
Andrew Philpott and Jack Dunning <andrew+jack@>
2020-03-10 08:47:09 -0700

    Add functionality to get list of cars and assign a car an Id.

--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -1,19 +1,22 @@
 namespace CarDealership.Models {
   public class Car
   {
-
+    public int Id{get;}
     public string Description {get;set;}
     public string MakeModel {get; set;}
     public int Price {get;set;}
     public int Miles {get;set;}
-    private static List<Cars> _cars = new List<Cars>();
-
+    private static List<Car> _cars = new List<Car>();
+    private static int CarId {get;} = 0;
     public Car(string description, string makeModel, int price, int miles)
     {
+      
       Description = description;
       MakeModel = makeModel;
       Price = price;
       Miles = miles;
+      ++CarId;
+      Id = CarId;
       _cars.Add(this);
     }
     public bool WorthBuying(int maxPrice, int miles)
@@ -23,6 +26,13 @@ namespace CarDealership.Models {
       } else {
         return false;
       }
+    }
+    public List<Car> GetAll(){
+        return _cars;
+    }
+
+    public bool RemoveCar(){
+
     }
   }
 }
